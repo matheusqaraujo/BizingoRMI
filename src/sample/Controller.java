d9a2cab9a77763a352d940219653656da6f94f17
Matheus Araujo <mathqah@gmail.com>
2020-02-03 23:15:41 -0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+    @FXML
+    public GridPane bizingoBoard;
+
+    public void TESTE_ALGUMA_COISA(){
+        System.out.println("FUNCIONOU");
+
+    }
+}
