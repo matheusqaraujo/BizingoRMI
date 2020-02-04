190c54da37be7fffeb23cc83893aff04926b0095
Matheus Araujo <mathqah@gmail.com>
2020-02-03 23:45:40 -0300

    Adicionando evento

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,21 @@
 package sample;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
 
 public class Controller {
     @FXML
     public GridPane bizingoBoard;
+    @FXML
+    public void TESTE_ALGUMA_COISA(Event event){
 
-    public void TESTE_ALGUMA_COISA(){
-        System.out.println("FUNCIONOU");
+        Node n = (Node)event.getSource();
+
+        String[] data = ((String)n.getUserData()).split("-");
+        System.out.println("LINHA: " + data[0]);
+        System.out.println("COLUNA: " + data[1]);
 
     }
 }
