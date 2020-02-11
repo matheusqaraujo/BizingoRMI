7d0ab88443a18dcdb49513cc16f5ed7fe9e260d8
Matheus Araujo <mathqah@gmail.com>
2020-02-10 22:27:37 -0300

    Adicionado movimentação das peças

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,6 +8,12 @@ import javafx.scene.layout.GridPane;
 public class Controller {
     @FXML
     public GridPane bizingoBoard;
+
+    Integer posicaoAntigaX = 0;
+    Integer posicaoAntigaY = 0;
+    boolean Flag = false;
+    Node origem;
+
     @FXML
     public void TESTE_ALGUMA_COISA(Event event){
 
@@ -17,8 +23,32 @@ public class Controller {
         //System.out.println("LINHA: " + data[0]);
         //System.out.println("COLUNA: " + data[1]);
 
-        System.out.println("Linha: "+ GridPane.getRowIndex(n));
-        System.out.println("Coluna: "+ GridPane.getColumnIndex(n));
+
+        if (Flag == false){
+            origem = n;
+
+            posicaoAntigaX = GridPane.getRowIndex(n);
+            posicaoAntigaY = GridPane.getColumnIndex(n);
+
+            System.out.println("Peça origem");
+            System.out.println("Linha: "+ GridPane.getRowIndex(n));
+            System.out.println("Coluna: "+ GridPane.getColumnIndex(n));
+
+            Flag = true;
+        }
+
+        else {
+            GridPane.setRowIndex(origem, GridPane.getRowIndex(n));
+            GridPane.setColumnIndex(origem, GridPane.getColumnIndex(n));
+            origem.setTranslateY(origem.getTranslateY() * -1);
+            System.out.println("Peça movimentada");
+            System.out.println("Linha: "+ posicaoAntigaX + " > " + GridPane.getRowIndex(n));
+            System.out.println("Coluna: "+ posicaoAntigaY + " > " + GridPane.getColumnIndex(n));
+
+            Flag = false;
+        }
+
+        System.out.println("----------------------");
 
     }
 }
